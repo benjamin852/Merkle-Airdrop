b8c6f4b35b782c9ea6b185be35dfc81dee4219e7
Ben Weinberg <weinbergben33@gmail.com>
2023-03-15 15:33:27 -0400

    compiling beginning of foundry merkle

--- test/foundry/MerkleAirdropERC20.t.sol
+++ test/foundry/MerkleAirdropERC20.t.sol
@@ -2,25 +2,32 @@
 pragma solidity ^0.8.18;
 
 import 'forge-std/Test.sol';
+import 'murky/Merkle.sol';
 
 // import './helper/Setup.sol';
 
-contract Setup is Test {
+contract Setup is Test, Merkle {
     uint256 NUM_LEAVES = 1000;
 
-    bytes32 public leafData;
+    bytes32[] public merkleTreeElements;
 
     function setUp() public {
         Merkle m = new Merkle();
 
-        for (uint i = 0; i < NUM_LEAVES; i++) {}
+        for (uint i = 0; i < NUM_LEAVES; i++) {
+            uint256 pseudoRandomAmount = uint256(
+                keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))
+            ) % i;
+            bytes32 leafData = _hashDataForLeaf(msg.sender, pseudoRandomAmount);
+            merkleTreeElements.push(leafData);
+        }
     }
 
     function _hashDataForLeaf(
         address _recipient,
         uint256 _tokenAmount
     ) private returns (bytes32 hashedData) {
-        hashedData = keccak256(_recipent, _tokenAmount);
+        hashedData = keccak256(abi.encodePacked(_recipient, _tokenAmount));
     }
 }
 

bc3eb92717953f0eeee2f9bafdc26ad545e6e6df
Ben Weinberg <weinbergben33@gmail.com>
2023-03-15 15:11:42 -0400

    setting up erc20 airdrop merkle tree

--- test/foundry/MerkleAirdropERC20.t.sol
+++ test/foundry/MerkleAirdropERC20.t.sol
@@ -5,6 +5,25 @@ import 'forge-std/Test.sol';
 
 // import './helper/Setup.sol';
 
+contract Setup is Test {
+    uint256 NUM_LEAVES = 1000;
+
+    bytes32 public leafData;
+
+    function setUp() public {
+        Merkle m = new Merkle();
+
+        for (uint i = 0; i < NUM_LEAVES; i++) {}
+    }
+
+    function _hashDataForLeaf(
+        address _recipient,
+        uint256 _tokenAmount
+    ) private returns (bytes32 hashedData) {
+        hashedData = keccak256(_recipent, _tokenAmount);
+    }
+}
+
 contract Initialization is Test {
     function setUp() public {
         console.log('hello!');

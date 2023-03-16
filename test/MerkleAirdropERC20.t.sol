040f95f524fb718b74b2459c7803128079b3c9d3
Ben Weinberg <weinbergben33@gmail.com>
2023-03-15 20:38:45 -0400

    first foundry test passing

--- test/MerkleAirdropERC20.t.sol
+++ test/MerkleAirdropERC20.t.sol
@@ -1,12 +1,12 @@
-// // SPDX-License-Identifier: Unlicense
-// pragma solidity ^0.8.18;
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.18;
 
-// import 'forge-std/Test.sol';
-// import './Setup.sol';
+import 'forge-std/Test.sol';
+import './Setup.sol';
 
-// contract Constructor is Setup {
-//     // function testShouldCorrectStorageVars() public returns (bool) {
-//     //     // MockERC20 tokenFromContract = merkleAirdrop.token();
-//     //     assertEq(5, 5);
-//     // }
-// }
+contract Constructor is Setup {
+    function testShouldCorrectStorageVars() public {
+        MockERC20 tokenFromContract = merkleAirdrop.token();
+        assertEq(address(tokenFromContract), address(token));
+    }
+}
--- test/Setup.sol
+++ test/Setup.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 import 'forge-std/Test.sol';
 import 'murky/Merkle.sol';
-import {FoundryRandom} from 'foundry-random/FoundryRandom.sol'; //forget test -ffi --> to allow for this script to be used
+import {FoundryRandom} from 'foundry-random/FoundryRandom.sol'; //forge test --ffi --> to allow for this script to be used
 import '../contracts/mocks/MockERC20.sol';
 import '../contracts/FungibleMerkleAirdrop.sol';
 
@@ -20,29 +20,23 @@ contract Setup is Test, Merkle, FoundryRandom {
     function setUp() public {
         Merkle m = new Merkle();
 
-        // for (uint i = 0; i < NUM_LEAVES; i++) {
-        //     uint256 pseudoRandomAmount = randomNumber(1, 10_000);
-        //     bytes32 leafData = _hashDataForLeaf(msg.sender, pseudoRandomAmount);
-        //     merkleTreeElements.push(leafData);
-        // }
+        for (uint i = 0; i < NUM_LEAVES; i++) {
+            uint256 pseudoRandomAmount = randomNumber(1, 10_000);
+            bytes32 leafData = _hashDataForLeaf(msg.sender, pseudoRandomAmount);
+            merkleTreeElements.push(leafData);
+        }
 
-        // bytes32 root = m.getRoot(merkleTreeElements);
+        bytes32 root = m.getRoot(merkleTreeElements);
 
-        // token = new MockERC20('BenERC20', 'B_ERC20', merkleRoot);
+        token = new MockERC20('BenERC20', 'B_ERC20', merkleRoot);
 
-        // merkleAirdrop = new FungibleMerkleAirdrop(token, merkleRoot);
-    }
-
-    function testShouldCorrectStorageVars() public returns (bool) {
-        // MockERC20 tokenFromContract = merkleAirdrop.token();
-        console.log(vm.addr(1));
-        assertEq(5, 5);
+        merkleAirdrop = new FungibleMerkleAirdrop(token, merkleRoot);
     }
 
     function _hashDataForLeaf(
         address _recipient,
         uint256 _tokenAmount
-    ) private returns (bytes32 hashedData) {
+    ) private pure returns (bytes32 hashedData) {
         hashedData = keccak256(abi.encodePacked(_recipient, _tokenAmount));
     }
 }
